dadce23f1fa93bf5e97c852d85dda4b8c41bb0d9
Adam Warner <adamw@rner.email>
2018-02-18 18:12:12 +0000

    allow user to pass in HttpRequest

--- GithubWebhook/GithubWebhook.cs
+++ GithubWebhook/GithubWebhook.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using GithubWebhook.Events;
+using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 
 namespace GithubWebhook
@@ -11,25 +14,61 @@ namespace GithubWebhook
         public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
         {
             MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-            DateParseHandling = DateParseHandling.None,
+            DateParseHandling = DateParseHandling.None
         };
     }
 
     public class GithubWebhook
     {
-        public string Event { get; }
-        public string Signature { get; }
+        [Obsolete("This can still be used, but it is preffered to pass in the HttpRequest instead.")]
+        public GithubWebhook(string strEvent, string signature, string delivery, string payloadText)
+        {
+            Event = strEvent;
+            Signature = signature;
+            Delivery = delivery;
+            PayloadText = payloadText;
+
+            PayloadObject = ConvertPayload();
+        }
+        
+        public GithubWebhook(HttpRequest hookIn)
+        {
+            hookIn.Headers.TryGetValue("X-GitHub-Event", out var strEvent);
+            hookIn.Headers.TryGetValue("X-Hub-Signature", out var signature);
+            hookIn.Headers.TryGetValue("X-GitHub-Delivery", out var delivery);
+            hookIn.Headers.TryGetValue("Content-type", out var content);
+
+            Event = strEvent;
+            Signature = signature;
+            Delivery = delivery;
+
+            if (content != "application/json")
+            {
+                throw new Exception("Invalid content type. Expected application/json");
+            }
+
+            using (var reader = new StreamReader(hookIn.Body, Encoding.UTF8))
+            {
+                PayloadText = reader.ReadToEnd();
+            }
+
+            PayloadObject = ConvertPayload();
+
+
+
+        }
+
         public string Delivery { get; }
-        public string CalculatedSignature { get; }
-        public bool SignatureValid { get; }
+        public string Event { get; }
         public object PayloadObject { get; }
+
+        private string PayloadText { get; }
+        public string Signature { get; }
 
         private static string ValidateSignature(string payload, string signatureWithPrefix, string secret)
         {
             if (!signatureWithPrefix.StartsWith("sha1=", StringComparison.OrdinalIgnoreCase))
-            {
                 return "Invalid shaPrefix";
-            }
 
             var secretBytes = Encoding.UTF8.GetBytes(secret);
             var payloadBytes = Encoding.UTF8.GetBytes(payload);
@@ -40,129 +79,97 @@ namespace GithubWebhook
 
                 return $"sha1={ToHexString(hash)}";
             }
-
         }
 
-        private static string ToHexString(byte[] bytes)
+        private static string ToHexString(IReadOnlyCollection<byte> bytes)
         {
-            var builder = new StringBuilder(bytes.Length * 2);
-            foreach (var b in bytes)
-            {
-                builder.AppendFormat("{0:x2}", b);
-            }
+            var builder = new StringBuilder(bytes.Count * 2);
+            foreach (var b in bytes) builder.AppendFormat("{0:x2}", b);
 
             return builder.ToString();
         }
 
-        public GithubWebhook(string strEvent, string signature, string delivery, string payloadText, string clientSecret)
+        private object ConvertPayload()
         {
-            Event = strEvent;
-            Signature = signature;
-            Delivery = delivery;
-
-            CalculatedSignature = ValidateSignature(payloadText, signature, clientSecret);
-
-            SignatureValid = CalculatedSignature == signature;
-
             switch (Event)
             {
                 case "ping":
-                    PayloadObject = PingEvent.FromJson(payloadText);
-                    break;
+                    return  PingEvent.FromJson(PayloadText);
                 case "commit_comment":
-                    PayloadObject = CommitCommentEvent.FromJson(payloadText);
-                    break;
+                    return  CommitCommentEvent.FromJson(PayloadText);
                 case "create":
-                    PayloadObject = CreateEvent.FromJson(payloadText);
-                    break;
+                    return  CreateEvent.FromJson(PayloadText);
                 case "delete":
-                    PayloadObject = DeleteEvent.FromJson(payloadText);
-                    break;
+                    return  DeleteEvent.FromJson(PayloadText);
                 case "deployment":
-                    PayloadObject = DeploymentEvent.FromJson(payloadText);
-                    break;
+                    return  DeploymentEvent.FromJson(PayloadText);
                 case "deployment_status":
-                    PayloadObject = DeploymentStatusEvent.FromJson(payloadText);
-                    break;
+                    return  DeploymentStatusEvent.FromJson(PayloadText);
                 case "fork":
-                    PayloadObject = ForkEvent.FromJson(payloadText);
-                    break;
+                    return  ForkEvent.FromJson(PayloadText);
                 case "gollum":
-                    PayloadObject = GollumEvent.FromJson(payloadText);
-                    break;
+                    return  GollumEvent.FromJson(PayloadText);
                 case "installation":
-                    PayloadObject = InstallationEvent.FromJson(payloadText);
-                    break;
+                    return  InstallationEvent.FromJson(PayloadText);
                 case "installation_repositories":
-                    PayloadObject = InstallationRepositoriesEvent.FromJson(payloadText);
-                    break;
+                    return  InstallationRepositoriesEvent.FromJson(PayloadText);
                 case "issue_comment":
-                    PayloadObject = IssueCommentEvent.FromJson(payloadText);
-                    break;
+                    return  IssueCommentEvent.FromJson(PayloadText);
                 case "issues":
-                    PayloadObject = IssuesEvent.FromJson(payloadText);
-                    break;
+                    return  IssuesEvent.FromJson(PayloadText);
                 case "label":
-                    PayloadObject = LabelEvent.FromJson(payloadText);
-                    break;
+                    return  LabelEvent.FromJson(PayloadText);
                 case "member":
-                    PayloadObject = MemberEvent.FromJson(payloadText);
-                    break;
+                    return  MemberEvent.FromJson(PayloadText);
                 case "membership":
-                    PayloadObject = MembershipEvent.FromJson(payloadText);
-                    break;
+                    return  MembershipEvent.FromJson(PayloadText);
                 case "milestone":
-                    PayloadObject = MilestoneEvent.FromJson(payloadText);
-                    break;
+                    return  MilestoneEvent.FromJson(PayloadText);
                 case "organization":
-                    PayloadObject = OrganizationEvent.FromJson(payloadText);
-                    break;
+                    return  OrganizationEvent.FromJson(PayloadText);
                 case "org_block":
-                    PayloadObject = OrgBlockEvent.FromJson(payloadText);
-                    break;
+                    return  OrgBlockEvent.FromJson(PayloadText);
                 case "page_build":
-                    PayloadObject = PageBuildEvent.FromJson(payloadText);
-                    break;
+                    return  PageBuildEvent.FromJson(PayloadText);
                 case "project_card":
-                    PayloadObject = ProjectCardEvent.FromJson(payloadText);
-                    break;
+                    return  ProjectCardEvent.FromJson(PayloadText);
                 case "project_column":
-                    PayloadObject = ProjectColumnEvent.FromJson(payloadText);
-                    break;
+                    return  ProjectColumnEvent.FromJson(PayloadText);
                 case "project":
-                    PayloadObject = ProjectEvent.FromJson(payloadText);
-                    break;
+                    return  ProjectEvent.FromJson(PayloadText);
                 case "public":
-                    PayloadObject = PublicEvent.FromJson(payloadText);
-                    break;
+                    return  PublicEvent.FromJson(PayloadText);
                 case "pull_request":
-                    PayloadObject = PullRequestEvent.FromJson(payloadText);
-                    break;
+                    return  PullRequestEvent.FromJson(PayloadText);
                 case "pull_request_review":
-                    PayloadObject = PullRequestReviewEvent.FromJson(payloadText);
-                    break;
+                    return  PullRequestReviewEvent.FromJson(PayloadText);
                 case "pull_request_review_comment":
-                    PayloadObject = PullRequestReviewCommentEvent.FromJson(payloadText);
-                    break;
+                    return  PullRequestReviewCommentEvent.FromJson(PayloadText);
                 case "push":
-                    PayloadObject = PushEvent.FromJson(payloadText);
-                    break;
+                    return  PushEvent.FromJson(PayloadText);
                 case "release":
-                    PayloadObject = ReleaseEvent.FromJson(payloadText);
-                    break;
+                    return  ReleaseEvent.FromJson(PayloadText);
                 case "repository":
-                    PayloadObject = RepositoryEvent.FromJson(payloadText);
-                    break;
+                    return  RepositoryEvent.FromJson(PayloadText);
                 case "status":
-                    PayloadObject = StatusEvent.FromJson(payloadText);
-                    break;
+                    return  StatusEvent.FromJson(PayloadText);
                 case "watch":
-                    PayloadObject = WatchEvent.FromJson(payloadText);
-                    break;
+                    return  WatchEvent.FromJson(PayloadText);
                 default:
-                    throw new NotImplementedException($"Event Type: `{Event}` is not implemented. Want it added? Open an issue at https://github.com/promofaux/GithubWebhooks");
+                    throw new NotImplementedException(
+                        $"Event Type: `{Event}` is not implemented. Want it added? Open an issue at https://github.com/promofaux/GithubWebhooks");
             }
+        }
 
+
+        public bool SignatureValid(string clientSecret)
+        {
+            return ValidateSignature(PayloadText, Signature, clientSecret) == Signature;
+        }
+
+        public string GetExpectedSignature(string clientSecret)
+        {
+            return ValidateSignature(PayloadText, Signature, clientSecret);
         }
     }
 }
