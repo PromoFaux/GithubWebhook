3eee07747e13a4272ee169639467846a623e943a
Adam Warner <adamw@rner.email>
2018-12-10 22:08:49 +0000

    forgot I had renamed `GithubWebhook` to `GhWebhook`, don't think anyone is using this library, but on the offchance they are...

--- GithubWebhook.Tests/EventTests.cs
+++ GithubWebhook.Tests/EventTests.cs
@@ -22,7 +22,7 @@ namespace GithubWebhook.Tests
             context.Request.Headers.Add("X-Github-Event", "push");
             context.Request.Body = ConvertResourceFileToMemoryStream("Push");
 
-            var hook = new GithubWebhook(context.Request);
+            var hook = new GhWebhook(context.Request);
             Assert.Equal("GithubWebhook.Events.PushEvent", hook.PayloadObject.GetType().ToString());
         }
 
@@ -34,7 +34,7 @@ namespace GithubWebhook.Tests
             context.Request.Headers.Add("X-Github-Event", "pull_request");
             context.Request.Body = ConvertResourceFileToMemoryStream("PullRequest");
 
-            var hook = new GithubWebhook(context.Request);
+            var hook = new GhWebhook(context.Request);
             Assert.Equal("GithubWebhook.Events.PullRequestEvent", hook.PayloadObject.GetType().ToString());
 
         }
